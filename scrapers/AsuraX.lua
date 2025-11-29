25fabf529299614c10f0551491542e25a3306872
kzero0855-gif <kzero0855@gmail.com>
2025-11-29 02:39:50 +0200

    Update AsuraX.lua

--- scrapers/AsuraX.lua
+++ scrapers/AsuraX.lua
@@ -39,7 +39,8 @@ function SearchManga(query)
     local mangas = {}
 
     for i, v in ipairs(Page:elements(".grid.grid-cols-2.sm\\:grid-cols-2.md\\:grid-cols-5.gap-3.p-4 > a")) do
-        local manga = { url = v:attribute('href'), name = v:attribute('href') }
+        local pgz = v:element(".block.text-\\[13\\.3px\\].font-bold")
+        local manga = { url = v:attribute('href'), name = pgz:inner_text() }
         mangas[i + 1] = manga
     end
 
